8015e158622ed9b0e0550a6f3cca725432b253c7
braedengoodwin <braedenagoodwin@hotmail.com>
2025-03-26 18:46:57 -0500

    on lecture 124, need to switch to new project from course

--- src/app/dashboard/server-status/status.component.ts
+++ src/app/dashboard/server-status/status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-status',
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
   templateUrl: './status.component.html',
   styleUrl: './status.component.css',
 })
-export class StatusComponent {
-  currentStatus = 'online';
+export class StatusComponent implements OnInit {
+  // what we're doing in currentStatus is known as "Literal Types", it's a typescript feature
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+
+  constructor() {}
+
+  ngOnInit() {
+    setInterval(() => {
+      const rnd = Math.random();
+
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
 }
